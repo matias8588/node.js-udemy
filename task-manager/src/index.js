b80197aea144ada778e034c9c55234f9b1c8af5d
Matias Cejas <matiascejas8@gmail.com>
2020-08-21 08:28:09 -0300

    Entities relationship

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -19,3 +19,17 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
 });
+
+const Task = require('./models/task');
+const User = require('./models/user');
+
+const main = async () => {
+  //   const task = await Task.findById('5f3fa7e3ebb55c1ff20ed5ca');
+  //   await task.populate('owner').execPopulate();
+  //   console.log(task.owner);
+  const user = await User.findById('5f3fa6d44e54931f5613789a');
+  await user.populate('tasks').execPopulate();
+  console.log(user.tasks);
+};
+
+main();
