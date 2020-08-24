5108d8bb470b597569e18eda96fa01ac065db781
Matias Cejas <matiascejas8@gmail.com>
2020-08-24 07:33:21 -0300

    Images upload

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,12 +6,6 @@ const taskRouter = require('./routes/task');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method) {
-//     res.status(503).send('Maintenance mode');
-//   }
-// });
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
@@ -19,17 +13,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server up on port ${port}`);
 });
-
-// const Task = require('./models/task');
-// const User = require('./models/user');
-
-// const main = async () => {
-//   const task = await Task.findById('5f3fa7e3ebb55c1ff20ed5ca');
-//   await task.populate('owner').execPopulate();
-//   console.log(task.owner);
-//   const user = await User.findById('5f3fa6d44e54931f5613789a');
-//   await user.populate('tasks').execPopulate();
-//   console.log(user.tasks);
-// };
-
-// main();
--- task-manager/src/routes/user.js
+++ task-manager/src/routes/user.js
@@ -2,6 +2,20 @@ const express = require('express');
 const User = require('../models/user');
 const router = new express.Router();
 const auth = require('../middlewares/auth');
+const multer = require('multer');
+const sharp = require('sharp');
+
+const upload = multer({
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('Please upload an image'));
+    }
+    cb(undefined, true);
+  },
+});
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body);
@@ -80,6 +94,52 @@ router.delete('/users/me', auth, async (req, res) => {
     res.send(req.user);
   } catch (error) {
     res.status(500).send();
+  }
+});
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  try {
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send(req.user);
+  } catch (error) {
+    res.status(500).send();
+  }
+});
+
+router.post(
+  '/users/me/avatar',
+  auth,
+  upload.single('avatar'),
+  async (req, res) => {
+    const buffer = await sharp(req.file.buffer)
+      .resize({
+        width: 250,
+        height: 250,
+      })
+      .png()
+      .toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send(req.user);
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  },
+);
+
+router.get('/users/:id/avatar', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user || !user.avatar) {
+      throw new Error();
+    }
+
+    res.set('Content-type', 'image/png');
+    res.send(user.avatar);
+  } catch (error) {
+    res.status(404).send();
   }
 });
 
